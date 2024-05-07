fb3749c14fa672b046cdcc79f8cc56c5c67f572b
knightofren2588 <knightofren2488@gmail.com>
2024-05-07 16:31:25 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// explaining my age below
+var myAge = '36';
+// early years is a saved value of 2 for now
+var earlyYears = 2 * 10.5;
+var laterYears= (myAge - 2) * 4;
+// math.floor(laterYears * 4);
+var myAgeinDogYears = (earlyYears + laterYears);
+var myName = 'MIKE';
+var Name = (myName.toLowerCase());
+console.log(`My Name is ${myName}. I am ${myAge} which is ${myAgeinDogYears} in dog years!`)
